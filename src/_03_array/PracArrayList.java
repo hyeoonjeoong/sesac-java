5867dc0a973d9d8efce3c7228a1ec737196a6c4b
hyeoonjeoong <jeoooooong22@gmail.com>
2024-01-18 18:40:01 +0900

    java array

---
+++ src/_03_array/PracArrayList.java
@@ -0,0 +1,13 @@
+package _03_array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PracArrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> textList = new ArrayList<>();
+
+    }
+}
